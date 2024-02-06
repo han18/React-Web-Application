1e2d80a95573db48444de06ffc7d7d0e48e9e81d
han18 <han.fadah@gmail.com>
2024-02-06 14:09:08 -0500

    created the useEffect function and added the logic

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  cons;
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          "https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/9999999999999999/books"
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        console.log(data);
+        setBooks(data.items);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+  }, []);
   return (
     <div className="App">
       <h1>This is a app</h1>
