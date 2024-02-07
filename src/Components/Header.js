dad7e661341dc7508b6a721802bd7a60c0ef1678
han18 <han.fadah@gmail.com>
2024-02-06 21:54:22 -0500

    created the navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,14 +17,6 @@ function Header() {
               menuVariant="dark"
             >
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
